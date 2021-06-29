fe498d003dfb291dae2b844436ba0816e5360c84
arthurfernndolucas <arthurlucasfernando@gmail.com>
2021-06-29 15:57:43 -0300

    Selection Sort

---
+++ Sorting-Algorithms/Sorting-Algorithms/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorting_Algorithms
+{
+    internal class Program
+    {
+       
+        public static void Main(string[] args)
+        {
+            // Selecton Sort
+            var selection = new SelectionSort();
+            var list = new List<int>(){10,9,8,7,6,5,5,7,3,6,5,56,1,23,34,54,23,54,3,9,4,3,2,1};
+            Console.WriteLine($"List: ({string.Join(", ", list)}).");
+            list =  selection.NumberOrdering(list);
+            Console.WriteLine($"Selection Sort list: ({string.Join(", ", list)}).");
+        }
+        
+        
+    }
+}
---
+++ Sorting-Algorithms/Sorting-Algorithms/SelectionSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Sorting_Algorithms
+{
+    public class SelectionSort
+    {
+        public List<int> NumberOrdering(List<int> list)
+        {
+            int min, aux;
+            for (int i = 0; i < list.Count; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < list.Count; j++)
+                    if (list[j] < list[min])
+                        min = j;
+
+
+                if (min == i) continue;
+                aux = list[min];
+                list[min] = list[i];
+                list[i] = aux;
+            }
+
+            return list;
+        }
+    }
+}
